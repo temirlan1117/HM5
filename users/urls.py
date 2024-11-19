455362bcf9f4702924e7f895a99e72b5be851fd0
temirlan1117 <temirlanormushev@yandex.ru>
2024-11-19 19:37:18 +0300

    HM5N2

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('/registration/', views.registration_view ),
-    path('/authorization/', views.authorization_view ),
+    path('registration/', views.registration_view ),
+    path('authorization/', views.authorization_view ),
     path('confirmation/', views.confirmation_view),
 
 ]
